d7345daf934990cc56c4dc5599268a9bc3551cab
Nischal Mainali <nm2508@nyu.edu>
2018-04-07 14:05:46 -0400

    initial commit

--- BIgramFeature.py
+++ BIgramFeature.py
@@ -8,8 +8,7 @@ from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
-import sklearn.svm
+
 
 
 ## const
@@ -33,7 +32,7 @@ d_data_set = loadDataset1()
 #splitting to pieces:
 c_list = ""
 for i in c_data_set:
-    c_list = c_list + i
+	c_list = c_list + i
 
 d_list=""
 for i in d_data_set:
@@ -43,23 +42,38 @@ c_data = []
 div = len(c_list)//100
 temp = ""
 for j in range(len(c_list)):
-    temp = temp + c_list[j]
-    if j%div == 0:
-        c_data.append(temp)
-        temp = ""
+	temp = temp + c_list[j]
+	if j%div == 0:
+		c_data.append(temp)
+		temp = ""
 c_data.append(temp)
 
 d_data = []
 div = len(d_list)//100
 temp = ""
 for j in range(len(d_list)):
-    temp = temp + d_list[j]
-    if j%div == 0:
-        d_data.append(temp)
-        temp = ""
+	temp = temp + d_list[j]
+	if j%div == 0:
+		d_data.append(temp)
+		temp = ""
 d_data.append(temp)
 
-final_data_set = c_data + d_data
+print(len(c_data), len(d_data))
+
+'''
+d_data = []
+div = len(d_data)//100
+temp = []
+for j in range(len(d_list)):
+	temp.append(d_list[j])
+	if j%div == 0:
+		d_data.append(temp)
+		temp = []
+d_data.append(temp)
+
+print(len(c_data), len(d_data))
+
+
 
 
 
@@ -68,53 +82,19 @@ final_data_set = c_data + d_data
 bigram_vectorizer = CountVectorizer(ngram_range=(1, 3),token_pattern=r'\b\w+\b', min_df=1)
 analyze = bigram_vectorizer.build_analyzer()
 #print(analyze(c_data_set[0]))
-X = bigram_vectorizer.fit_transform(final_data_set).toarray()
+X_1 = bigram_vectorizer.fit_transform(c_data_set).toarray()
+X_2 = bigram_vectorizer.fit_transform(d_data_set).toarray()
+X_train = X_1.tolist() + X_2.tolist()
 
 y = []
-for i in c_data:
-    y.append('cons')
-for i in d_data:
-    y.append('deon')
+for i in X_1:
+	y.append('cons')
+for i in X_2:
+	y.append('deon')
 
-
-print(len(X))
+print(len(X_1),len(X_2),len(X_train))
 print(y)
+'''
 
 
-
-
-#------------
-
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 50)
-
-
-clf = MultinomialNB().fit(X_train, y_train)
-predicted = clf.predict(X_test)
-
-n = 0
-correct = 0
-for i, j in zip(y_test, predicted):
-    print('%r => %s' % (i, j))
-    n = n + 1
-    if i == j:
-        correct = correct + 1
-print(correct*100/n)
-
-
-
-
-print("SVM with Vector Featues")
-
-clf = sklearn.svm.LinearSVC().fit(X_train, y_train)
-predicted = clf.predict(X_test)
-
-n = 0
-correct = 0
-for i, j in zip(y_test, predicted):
-    print('%r => %s' % (i, j))
-    n = n + 1
-    if i == j:
-        correct = correct + 1
-print(correct*100/n)
 
